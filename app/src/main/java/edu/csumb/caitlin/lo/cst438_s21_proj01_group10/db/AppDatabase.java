459578794c26989fb03d4d158d29ea161121e494
lomcaitlin <lomcaitlin@gmail.com>
2021-02-04 13:37:18 -0800

    Added favorites table & updated DAO

--- app/src/main/java/edu/csumb/caitlin/lo/cst438_s21_proj01_group10/db/AppDatabase.java
+++ app/src/main/java/edu/csumb/caitlin/lo/cst438_s21_proj01_group10/db/AppDatabase.java
@@ -2,12 +2,14 @@ package edu.csumb.caitlin.lo.cst438_s21_proj01_group10.db;
 
 import androidx.room.*;
 
+import edu.csumb.caitlin.lo.cst438_s21_proj01_group10.db.tables.Favorites;
 import edu.csumb.caitlin.lo.cst438_s21_proj01_group10.db.tables.User;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, Favorites.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public static final String DB_NAME = "APP_DATABASE";
     public static final String USER_TABLE = "USER_TABLE";
+    public static final String FAVORITES_TABLE = "FAVORITES_TABLE";
 
     public abstract AppDAO getAppDAO();
 }
