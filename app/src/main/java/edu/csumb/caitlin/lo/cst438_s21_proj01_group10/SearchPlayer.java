9a15ce82bb7e0ee3487a4bce4a12919cfd7f41fa
lomcaitlin <lomcaitlin@gmail.com>
2021-02-12 22:53:02 -0800

    Added intent factory

--- app/src/main/java/edu/csumb/caitlin/lo/cst438_s21_proj01_group10/SearchPlayer.java
+++ app/src/main/java/edu/csumb/caitlin/lo/cst438_s21_proj01_group10/SearchPlayer.java
@@ -2,6 +2,8 @@ package edu.csumb.caitlin.lo.cst438_s21_proj01_group10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SearchPlayer extends AppCompatActivity {
@@ -10,5 +12,19 @@ public class SearchPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_player);
+
+
+
+    }
+
+    /**
+     * Intent factory to switch to SearchPlayer
+     * @param context
+     * @return
+     */
+    public static Intent getIntent(Context context) {
+        Intent intent = new Intent(context, SearchPlayer.class);
+        // intent.putExtra();
+        return intent;
     }
 }
